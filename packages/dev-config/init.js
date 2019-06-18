d9a216e8b91dca569d971f24d44cf785ca4383fa
Alex Ketch <alex@ketch.me>
2019-06-18 12:33:44 -0400

    chore(Package): Add post-install script for setting up Husky & Prettier

---
+++ packages/dev-config/init.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const writePkg = require('write-pkg')
+
+const cwd = process.env.INIT_CWD
+const pkgPath = path.join(cwd, 'package.json')
+
+const readPkg = async () =>
+  new Promise((resolve, reject) =>
+    fs.readFile(pkgPath, (err, data) => (err ? reject(err) : resolve(data)))
+  )
+
+const mergeConfigs = (config, newConfig) => ({
+  ...config,
+  ...newConfig
+})
+
+// New configurations to merge in to the project's `package.json`
+const prettierConfig = {
+  prettier: '@stencila/dev-config/prettier-config.json'
+}
+
+const husky = {
+  husky: {
+    hooks: {
+      'pre-commit': 'pretty-quick --staged',
+      'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS'
+    }
+  }
+}
+
+const esLint = {}
+
+const semanticRelease = {}
+
+const configurations = [prettierConfig, husky, esLint, semanticRelease]
+
+// Update the project's `package.json`
+;(async () => {
+  const pkg = JSON.parse(await readPkg())
+  const updatedPkg = configurations.reduce(mergeConfigs, pkg)
+
+  writePkg(cwd, updatedPkg)
+})()
