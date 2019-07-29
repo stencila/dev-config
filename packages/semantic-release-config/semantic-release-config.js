1fbd2bc311ab11b09a078ba4d1170b5f74b16e23
Nokome Bentley <nokome@stenci.la>
2019-07-29 12:35:07 +1200

    fix(Release): Run NPM plugin before Git plugin
    Closes #5

--- packages/semantic-release-config/semantic-release-config.js
+++ packages/semantic-release-config/semantic-release-config.js
@@ -3,8 +3,10 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
+    // `npm` must come before `git` so that updates to `package.json`
+    // and `package-lock.json` are committed.
+    '@semantic-release/npm',
     '@semantic-release/git',
-    '@semantic-release/github',
-    '@semantic-release/npm'
+    '@semantic-release/github'
   ]
 }
