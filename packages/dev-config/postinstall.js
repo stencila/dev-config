ea3a95a2a8e26ab202d2d928fa33b88950816d8c
Nokome Bentley <nokome@stenci.la>
2019-12-04 08:49:53 +1300

    feat(Post install): Make init.js bin script, improve message
    Closes #10.

---
+++ packages/dev-config/postinstall.js
@@ -0,0 +1,15 @@
+const reset = "\x1b[0m"
+const bright = "\x1b[1m"
+
+const green = "\x1b[32m"
+const blue = "\x1b[34m"
+const cyan = "\x1b[36m"
+
+console.log(`
+${bright}${green}@stencila/dev-config${reset}
+
+${blue}If this is the first time you’re installing this package,
+or wish to upgrade your configurations to a new version, run:${reset}
+
+${cyan}npx stencila-dev-config${reset}
+`)
