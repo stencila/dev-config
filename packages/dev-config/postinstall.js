9addb240c870e948d9584c8b4686d8e8b4e780d3
Nokome Bentley <nokome@stenci.la>
2019-12-06 09:58:32 +1300

    chore(Postinstall message):  Remove line at start, add prompt indicator

--- packages/dev-config/postinstall.js
+++ packages/dev-config/postinstall.js
@@ -1,15 +1,14 @@
-const reset = "\x1b[0m"
-const bright = "\x1b[1m"
+const reset = '\x1b[0m'
+const bright = '\x1b[1m'
 
-const green = "\x1b[32m"
-const blue = "\x1b[34m"
-const cyan = "\x1b[36m"
+const green = '\x1b[32m'
+const blue = '\x1b[34m'
+const cyan = '\x1b[36m'
 
-console.log(`
-${bright}${green}@stencila/dev-config${reset}
+console.log(`${bright}${green}@stencila/dev-config${reset}
 
 ${blue}If this is the first time you’re installing this package,
 or wish to upgrade your configurations to a new version, run:${reset}
 
-${cyan}npx stencila-dev-config${reset}
+${cyan}> npx stencila-dev-config${reset}
 `)
